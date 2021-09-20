4d6d56a64d8eaa856c77284cf678435dd547a5f2
Tania Alcudia <tanialcudia@gmail.com>
2021-09-20 08:48:26 +0200

    Ejercicio 1-7 Terminados

--- tema_01/tanda_01/tema1_tanda1_07.php
+++ tema_01/tanda_01/tema1_tanda1_07.php
@@ -1,8 +1,40 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tanda 1</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Ejercicio 7</h1>
+    </header>
+
+    <main>
+        <ul>
+        <?php
+            $handle = fopen("url/urls.txt", "r");
+            while (!feof($handle)) 
+            {
+                $linea = fgets($handle); //Va dejando en $linea cada linea del fichero. Al leer con fgets tener en cuenta que también se leen los caracteres de fin de línea
+                $partes = explode(" ", $linea);
+                echo "<li>$partes[0] <a href='$partes[1]'>$partes[1]</a></li>";
+            }
+            fclose($handle);
+        ?>
+        </ul>
+    </main>
+
+    <footer>
+    </footer>
+
+</body>
+
+</html>
