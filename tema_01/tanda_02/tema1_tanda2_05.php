47f3d20c67270a30f8a9119859518776b3b26591
Tania Alcudia <tanialcudia@gmail.com>
2021-09-28 14:32:28 +0200

    010205 CASI TERMINADO

---
+++ tema_01/tanda_02/tema1_tanda2_05.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+
+    define("FICH", "./bd/conversiones.txt");
+    define("EUROADOLAR", 1.16979);
+    define("DOLARAEURO", 0.854853);
+    
+    function convertir($cant, $opcion)
+    {
+        if ($opcion == "euroAdolar")
+             return $cant * EUROADOLAR;
+        else
+            return $cant * DOLARAEURO;
+    }
+
+    $botonPulsado = (isset($_POST['convertir']));
+    
+    if ($botonPulsado)
+    {
+        $campoVacio = (empty($_POST['cantidad']));
+        if (!$campoVacio)
+            $esNumero = is_numeric($_POST['cantidad']);
+    }
+
+    $cant = isset($_POST['cantidad']) ?  $_POST['cantidad'] : '';
+
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+        <table>
+            <tr>
+                <td>
+                    <label for="cantidad">Cantidad: </label>
+                    <input type="text" name="cantidad" id="cantidad" value="<?php echo $cant ?>">
+                </td>
+                <?php
+                    if ($botonPulsado)
+                        if ($campoVacio)
+                            echo "<td><p style='color:red'>¡VACIO!</p></td>";
+                        elseif (!$esNumero)
+                            echo "<td><p style='color:red'>¡NO NÚMERICO!</p></td>"; 
+                ?>
+                <td>
+                    <input type="radio" id="euroAdolar" name="tipoCambio" value="euroAdolar" checked>
+                    <label for="euroAdolar">Euros a dólares</label><br>
+                    <input type="radio" id="dolarAeuro" name="tipoCambio" value="dolarAeuro">
+                    <label for="dolarAeuro">Dólares a euros</label>
+                </td>
+            </tr>
+            <?php
+                if ($botonPulsado and $campoVacio == false and $esNumero)
+                    echo "<tr><td><p style='font-size:30px; margin:0px'>".convertir($_POST['cantidad'], $_POST['tipoCambio'])."</p></td></tr>";
+            ?>
+            <tr>
+                <td><input type="submit" name="convertir" value="CONVERTIR"></td>
+            </tr>
+        </table>
+        
+    </form>
+</body>
+</html>
